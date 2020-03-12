e7aacdfddc2aef25a1950977ae3269348166903e
darot <darotudeen@gmail.com>
2020-03-12 14:10:06 -0700

    added started files and background for home

--- myrootsinafrica/ViewController.swift
+++ myrootsinafrica/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let backgroundImageView = UIImageView()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setBackground()
     }
 
-
+    
+    func setBackground(){
+                view.addSubview(backgroundImageView)
+                backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+                backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+                backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+                backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+                backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+                backgroundImageView.image = UIImage(named: "home_background")
+                view.sendSubviewToBack(backgroundImageView)
+    }
 }
 
