ac8b8030624ff5c7d9f89986bd2056fd37b52cf6
darot <darotudeen@gmail.com>
2020-03-18 23:57:52 +0100

    added verification result secene

---
+++ myrootsinafrica/controllers/VerificationResultViewController.swift
@@ -0,0 +1,22 @@
+//
+//  VerificationResultViewController.swift
+//  myrootsinafrica
+//
+//  Created by Darot on 18/03/2020.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class VerificationResultViewController: UIViewController {
+    
+    @IBOutlet weak var viewSubContainer: UIView!
+    override func viewDidLoad() {
+        print("yeske")
+        
+        self.setBackgroundImage("verificationResultBackground", contentMode: .scaleToFill)
+        
+        viewSubContainer.setViewShadow(using: 4, color: UIColor.black.cgColor)
+        viewSubContainer.layer.cornerRadius = 10
+    }
+}
