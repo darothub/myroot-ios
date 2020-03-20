8cd72c086577f2ce7c3c0029c05e6025895759f6
darot <darotudeen@gmail.com>
2020-03-20 21:58:27 +0100

    added where to plant roots scene

---
+++ myrootsinafrica/controllers/WhereToPlantViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WhereToPlantViewController.swift
+//  myrootsinafrica
+//
+//  Created by Darot on 20/03/2020.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class WhereToPlantViewController:ViewController{
+    
+    
+    override func viewDidLoad() {
+        
+        self.setupProgressBar(progress: 0.2)
+    }
+}
