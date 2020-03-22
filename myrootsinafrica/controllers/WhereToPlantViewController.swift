f28283a48eac89eeb9bda94e48671c755bb5eb67
darot <darotudeen@gmail.com>
2020-03-22 13:28:50 +0100

    added selector to where to plantscene

--- myrootsinafrica/controllers/WhereToPlantViewController.swift
+++ myrootsinafrica/controllers/WhereToPlantViewController.swift
@@ -7,12 +7,75 @@
 //
 
 import UIKit
+import iOSDropDown
 
 class WhereToPlantViewController:ViewController{
     
+    @IBOutlet weak var countryIcon: UIImageView!
     
+    @IBOutlet weak var selectorCardForGGW: UIView!
+    @IBOutlet weak var selectorCardForCountry: UIView!
+    
+    @IBOutlet weak var fiftyFourCountriesDropDown: DropDown!
+    var fiftyFourCountries:[String] = []
     override func viewDidLoad() {
         
         self.setupProgressBar(progress: 0.2)
+        
+
+        fiftyFourCountriesDropDown.isEnabled = false
+       
+        fiftyFourCountries = ["Nigeria", "Zambia", "Ethiopia", "Ghana", "Egypt", "Senegal", "Sierra Leone", "South Africa"].sorted()
+        
+        fiftyFourCountriesDropDown.optionArray = fiftyFourCountries
+        
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(self.tapDetected))
+        selectorCardForCountry.isUserInteractionEnabled = true
+        selectorCardForCountry.addGestureRecognizer(singleTap)
+        
+        let singleTapForGGW = UITapGestureRecognizer(target: self, action: #selector(self.tapDetectedForGGWCard))
+        selectorCardForGGW.isUserInteractionEnabled = true
+        selectorCardForGGW.addGestureRecognizer(singleTapForGGW)
+        
+    }
+    
+    @objc func tapDetected()-> Bool{
+        
+        if showSelectorCard(view: selectorCardForCountry) {
+            fiftyFourCountriesDropDown.isEnabled = true
+            unSelectCard(view: selectorCardForGGW)
+            return true
+        }else{
+           return false
+        }
+
+        
+
+    }
+    
+    @objc func tapDetectedForGGWCard()-> Bool{
+        if showSelectorCard(view: selectorCardForGGW) {
+            fiftyFourCountriesDropDown.isEnabled = false
+            unSelectCard(view: selectorCardForCountry)
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    func showSelectorCard(view:UIView) -> Bool{
+        view.backgroundColor = .white
+        view.setViewShadow(using: 4, color: UIColor.black.cgColor)
+        view.layer.cornerRadius = 5
+        
+        return true
+    }
+    
+    func unSelectCard(view:UIView) -> Bool{
+        view.backgroundColor = .clear
+        view.setViewShadow(using: 0, color: UIColor.black.cgColor)
+        view.layer.cornerRadius = 0
+        
+        return true
     }
 }
