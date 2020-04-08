56e00dd3d68ba107a4857bf492916312e9e9f715
darot <darotudeen@gmail.com>
2020-04-08 01:33:12 +0100

    added dashboard report boards

--- myrootsinafrica/controllers/auth/DashBoardViewController.swift
+++ myrootsinafrica/controllers/auth/DashBoardViewController.swift
@@ -21,19 +21,31 @@ class DashBoardViewController : ViewController{
     
     @IBOutlet weak var greetingLabel: UILabel!
     @IBOutlet weak var reserveTreeTap: UIView!
-    @IBOutlet weak var topBoardView: UIView!
+//    @IBOutlet weak var topBoardView: UIView!
     @IBOutlet weak var countryBigCount: UILabel!
     @IBOutlet weak var ggwBigCount: UILabel!
     @IBOutlet weak var countriesTreesDetailLabel: UILabel!
     @IBOutlet weak var ggwTreesDetailsLabel: UILabel!
-    @IBOutlet weak var bottomBoardView: UIView!
+//    @IBOutlet weak var bottomBoardView: UIView!
     var tokens = ""
     var user:User?
     var userData:UserData?
-    
+    lazy var viewHeight = containerView.frame.height
     lazy var circleView = self.createCustomView(with: .white, height: 50, width: 50)
     lazy var profileIcon = self.createImageView(with: #imageLiteral(resourceName: "profileicon"))
-    lazy var viewHeight = containerView.frame.height
+    
+    lazy var topBoardView = self.createCustomView(with: .white, height: viewHeight/8, width: 0)
+    lazy var topBoardViewImage = self.createImageView(with: #imageLiteral(resourceName: "topboardLeaf"))
+    lazy var countryLabel = self.createUIlabelBold(with: NSLocalizedString("countryLabel", comment: "54 countries"), and: 16.0, color: #colorLiteral(red: 0.7607843137, green: 0.862745098, blue: 0, alpha: 1))
+    lazy var countryCountLabel = self.createUIlabelBold(with: NSLocalizedString("number", comment: "tree count"), and: viewHeight/18, color: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+    lazy var topBoardViewAdvice = self.createUIlabel(with: NSLocalizedString("topBoardAdvice", comment: "transaction update"), and: 14, color: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+    
+    lazy var bottomBoardView = self.createCustomView(with: .white, height: viewHeight/8, width: 0)
+    lazy var bottomBoardViewImage = self.createImageView(with: #imageLiteral(resourceName: "bottomboardLeaf"))
+    lazy var ggwLabel = self.createUIlabelBold(with: NSLocalizedString("ggwLabel", comment: "Great green wall"), and: 16.0, color: #colorLiteral(red: 0.7607843137, green: 0.862745098, blue: 0, alpha: 1))
+    lazy var ggwCountLabel = self.createUIlabelBold(with: NSLocalizedString("number", comment: "tree count"), and: viewHeight/18, color: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+    lazy var bottomBoardViewAdvice = self.createUIlabel(with: NSLocalizedString("bottomBoardAdvice", comment: "transaction update"), and: 14, color: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+   
     var tree:Tree?
     @IBOutlet weak var logOutButton: UIBarButtonItem!
     var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
@@ -56,8 +68,8 @@ class DashBoardViewController : ViewController{
         
         circleView.layer.cornerRadius = 25
         
-//        topBoardView.layer.cornerRadius = 25
-//        bottomBoardView.layer.cornerRadius = 25
+        topBoardView.layer.cornerRadius = 25
+        bottomBoardView.layer.cornerRadius = 25
 //
 //        self.navigationItem.setHidesBackButton(true, animated: true)
 //        circleView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapDetectedForProfile(_ :))))
@@ -226,8 +238,11 @@ class DashBoardViewController : ViewController{
         view.addSubview(self.scrollView)
         scrollView.edgesToSuperview()
         scrollView.addSubview(self.containerView)
-        customAddToSubView(parent:self.containerView, views: circleView)
+        customAddToSubView(parent:self.containerView, views: circleView, topBoardView, bottomBoardView)
         circleView.addSubview(profileIcon)
+        customAddToSubView(parent: topBoardView, views: topBoardViewImage, countryLabel, countryCountLabel, topBoardViewAdvice)
+        customAddToSubView(parent: bottomBoardView, views: bottomBoardViewImage, ggwLabel, ggwCountLabel, bottomBoardViewAdvice)
+
     }
     
     private func setViewConstraints() {
@@ -237,5 +252,37 @@ class DashBoardViewController : ViewController{
         profileIcon.size(CGSize(width: 30, height: 30))
         profileIcon.contentMode = .scaleAspectFit
         profileIcon.center(in: circleView)
+        
+        boardContainerViews(view: topBoardView, centerAnchorView: containerView, topAnchorView: circleView, offset: 60)
+        boardContainerViews(view: bottomBoardView, centerAnchorView: containerView, topAnchorView: topBoardView, offset: viewHeight/7)
+        setBoardsConstraint(parent: topBoardView, image: topBoardViewImage, header: countryLabel, number: countryCountLabel, advice: topBoardViewAdvice)
+        setBoardsConstraint(parent: bottomBoardView, image: bottomBoardViewImage, header: ggwLabel, number: ggwCountLabel, advice: bottomBoardViewAdvice)
+        
+        
+    }
+    
+    
+    private func boardContainerViews(view:UIView, centerAnchorView:UIView, topAnchorView:UIView, offset:CGFloat){
+        view.centerX(to: centerAnchorView)
+        view.top(to: topAnchorView, offset: offset, isActive: true)
+    }
+    
+    private func setBoardsConstraint(parent:UIView, image:UIImageView, header:UILabel, number:UILabel, advice:UILabel){
+        
+        parent.right(to: self.containerView, offset: -20, isActive: true)
+        parent.left(to: self.containerView, offset: 20, isActive: true)
+        
+        image.size(CGSize(width: viewHeight/14, height: viewHeight/14))
+        image.left(to: parent, offset: 10, isActive: true)
+        image.centerY(to: parent)
+        header.top(to: parent, offset: 10, isActive: true)
+        header.left(to: image, offset: 70, isActive: true)
+        number.left(to: image, offset: 70, isActive: true)
+        number.top(to: header, offset: 15, isActive: true)
+        advice.numberOfLines = 0
+        advice.textAlignment = .left
+        advice.left(to: number, offset: 35, isActive: true)
+        advice.top(to: header, offset: 20, isActive: true)
+        
     }
 }
